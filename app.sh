0464da91a32aeb75d42000d928beca72de806a75
Adrian Cueto <adrianjpca@gmail.com>
2024-06-05 11:32:51 -0600

    WiFi connection is added automatically

--- app.sh
+++ app.sh
@@ -34,7 +34,6 @@ sudo mkdir -p /usr/crank/apps/ProServices
 log_message "Descomprimiendo archivo linux-imx8yocto-armle-opengles" 
 sudo unzip /media/usb/linux/linux-imx8yocto-armle-opengles_2.0-7.0-40118.zip -d /usr/crank/runtimes/
 
-
 # 4. Asignar permisos 775 a runtimes y apps
 log_message "Asignnando permisos 775 al directorio runtimes y apps"
 sudo chmod -R 775 /usr/crank/runtimes /usr/crank/apps
@@ -52,40 +51,62 @@ log_message "Copiando servicios a sus respectivos directorios"
 sudo cp -f /media/usb/services/storyboard_splash.service /etc/systemd/system/
 sudo cp -f /media/usb/services/storyboard.service /etc/systemd/system/
 sudo cp -f /media/usb/services/combi_backend.service /lib/systemd/system/
+sudo cp -f /media/usb/services/wired.network /etc/systemd/network/
+sudo cp -f /media/usb/services/wireless.network /etc/systemd/network/
+sudo cp -f /media/usb/services/wpa_supplicant@wlan0.service /etc/systemd/system/
 
 # 8. Asignar permisos a los servicios
 log_message "Asignando permisos a los servicios" 
 sudo chmod 0644 /etc/systemd/system/storyboard_splash.service
 sudo chmod 0777 /etc/systemd/system/storyboard.service
 sudo chmod 0644 /lib/systemd/system/combi_backend.service
+sudo chmod 0644 /etc/systemd/network/wired.network
+sudo chmod 0644 /etc/systemd/network/wireless.network
+sudo chmod 0644 /etc/systemd/system/wpa_supplicant@wlan0.service
 
-# 9. Activar servicios
+# 9 Remover los manejadores de conexiones 
+sudo rm -f /etc/resolv.conf
+sudo rm -f /etc/tmpfiles.d/connman_resolvconf.conf
+sudo systemctl stop connman
+sudo systemctl stop connman-env
+sudo systemctl disable connman
+sudo systemctl disable connman-env
+
+#10. Activar servicios
 log_message "Activando servicios"
+sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+sudo systemctl stop wpa_supplicant
+sudo systemctl disable wpa_supplicant
+
 sudo systemctl daemon-reload
 sudo systemctl enable storyboard_splash.service
 sudo systemctl enable storyboard.service
 sudo systemctl enable combi_backend.service
+sudo systemctl enable wpa_supplicant@wlan0.service
+sudp systemctl enable systemd-resolved.service
+
 sudo systemctl start storyboard_splash.service
 sudo systemctl start storyboard.service
 sudo systemctl start combi_backend.service
 
-# 10. Cambiar el nombre del servicio weston
+
+#11. Cambiar el nombre del servicio weston
 log_message "Cambiando el nombre del servicio weston" 
 if [ -e "/lib/systemd/system/weston.service"];then #verifica si existe el archivo
     sudo mv /lib/systemd/system/weston.service /lib/systemd/system/weston_Pro_S.service
 else 
     log_message "El archivo ya fue cambiado."
 
-# 11. Copiar contenido de la carpeta "Application" a /usr/crank/apps/Pro-Services"
+#12. Copiar contenido de la carpeta "Application" a /usr/crank/apps/Pro-Services"
 log_message "Copiando la versión $version a la carpeta apps" 
 sudo cp -f -r /media/usb/app/* /usr/crank/apps/ProServices/
 sudo cp -f -r /media/usb/app/$version/* /usr/crank/apps/ProServices/
 
 
-# 12. Cambiar el logo de arranque
+#13. Cambiar el logo de arranque
 echo "Cambiando el logo de arranque"
 cp -f /media/usb/img/logo.bmp /run/media/mmcblk2p1/logo.bmp
 
-#13. Reiniciar
+#14. Reiniciar
 sudo reboot
 
