c08b203baed8d3acf6fdc939a0be454f6ce23869
Adrian Cueto <adrianjpca@gmail.com>
2024-06-06 05:39:18 -0600

    update script app.sh

--- app.sh
+++ app.sh
@@ -1,170 +1,163 @@
 #!/bin/bash
 
-# usb_ovenapp
-# 
-# Descripción:
-# Este script se utiliza para actualizar o revertir (rollback) la aplicación en la placa Forlinx.
-# 
-# Uso:
-# ./app.sh update
-# ./app.sh rollback <versión_del_software>
-# 
-# Ejemplos:
-# ./app.sh update                # Actualiza a la última versión
-# ./app.sh rollback 1.5.2        # Revierte a la versión 1.5.2
-# 
-# Nota:
-# Asegúrese de que la memoria USB está montada en /media/usb y que el script tiene permisos de ejecución.
+# usb_combioven
 #
-# Dependencias:
-# - sudo
-# - unzip
-# Autor: 
+# Description:
+# This script updates or rolls back the application on the Forlinx board. It automates
+# the process of copying application files, setting permissions, and configuring system
+# services to ensure a seamless update or rollback.
+#
+# Usage:
+# ./app.sh update
+# ./app.sh rollback <software_version>
+#
+# Examples:
+# ./app.sh update                # Updates to the latest version available on the USB drive
+# ./app.sh rollback 1.5.2        # Rolls back to version 1.5.2
+#
+# Note:
+# Ensure the USB drive is mounted at /media/usb and that the script has execution permissions.
+# This script requires 'sudo' privileges to execute certain commands.
+#
+# Dependencies:
+# - sudo: To execute commands with superuser privileges
+# - unzip: To extract application archives
+#
+# Author:
 # Jose Adrian Perez Cueto
 # adrianjpca@gmail.com
 ##
 
-# Archivo de registro
+# Variables
 LOG_FILE="/var/log/usboven.log"
 APP_PATH="/media/usb/app"
 APP_DEST="/usr/crank/apps/ProServices"
 LATEST_VERSION=$(ls -v "$APP_PATH" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | tail -n 1)
 
-# Función para registrar mensajes en el archivo de registro
+# Function to log messages to the log file
 log_message() {
     local message="$1"
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" | tee -a "$LOG_FILE"
 }
 
-# Limpiar el archivo de log al iniciar
+# Clear the log file at the start
 > "$LOG_FILE"
 
-if [ $# -eq 0 ]; then
-    echo "Error: Debe especificar 'update' o 'rollback <versión_del_software>' como argumento."
-    echo "Uso: $0 update | rollback <versión_del_software>"
+# Check arguments
+if [[ $# -eq 0 ]]; then
+    log_message  "Error: You must specify 'update' or 'rollback <software_version>' as an argument."
+    log_message  "Usage: $0 update | rollback <software_version>"
     exit 1
 fi
 
 operation=$1
 version=$2
 
-if [ "$operation" != "update" ] && [ "$operation" != "rollback" ]; then
-    echo "Error: Operación no válida. Use 'update' o 'rollback <versión_del_software>'."
+
+if [[ "$operation" != "update" && "$operation" != "rollback" ]]; then
+    echo "Error: Invalid operation. Use 'update' or 'rollback <software_version>'."
     exit 1
 fi
 
-if [ "$operation" = "rollback" ] && [ -z "$version" ]; then
-    echo "Error: Debe especificar la versión del software para rollback."
-    echo "Uso: $0 rollback <versión_del_software>"
+if [[ "$operation" == "rollback" && -z "$version" ]]; then
+    echo "Error: You must specify the software version for rollback."
+    echo "Usage: $0 rollback <software_version>"
     exit 1
 fi
 
+log_message "Starting the application transfer..."
 
-log_message  "Starting whith the app transfer..."
+# Create necessary directories
+log_message "Creating directory structure..."
+sudo mkdir -p /usr/crank/apps /usr/crank/runtimes /usr/crank/apps/ProServices
 
-# 1. Crear carpeta "crank" en /usr
-log_message "Creando carpeta crank en /usr..."
-sudo mkdir -p /usr/crank
-
-# 2. Crear carpetas "apps" y "runtimes" dentro de "crank"
-log_message "Creando carpetas apps y runtime dentro de crank..."
-sudo mkdir -p /usr/crank/apps /usr/crank/runtimes
-sudo mkdir -p /usr/crank/apps/ProServices
-
-# 3. Copiar archivo zip a runtimes y descomprimirlo
-log_message "Descomprimiendo archivo linux-imx8yocto-armle-opengles..." 
+# Unzip file into runtimes
+log_message "Unzipping linux-imx8yocto-armle-opengles file..."
 sudo unzip -o /media/usb/linux/linux-imx8yocto-armle-opengles_2.0-7.0-40118.zip -d /usr/crank/runtimes/
 
-# 4. Asignar permisos 775 a runtimes y apps
-log_message "Asignnando permisos 775 al directorio runtimes y apps..."
+# Set permissions
+log_message "Setting 0775 permissions for runtimes and apps..."
 sudo chmod -R 775 /usr/crank/runtimes /usr/crank/apps
 
-# 5. Copiar scripts a /usr/crank
-log_message "Copiando scripts a /usr/crank"
+# Copy scripts
+log_message "Copying scripts to /usr/crank..."
 sudo cp -f -r /media/usb/scripts/* /usr/crank/
-
-# 6. Asignar permisos 0755 a los scripts
-log_message "Asignando permisos 644 a los scripts..."
 sudo chmod 775 /usr/crank/*
 
-# 7. Copiar servicios a sus respectivos directorios
-log_message "Copiando servicios a directorios..." 
-sudo cp -f /media/usb/services/storyboard_splash.service /etc/systemd/system/
-sudo cp -f /media/usb/services/storyboard.service /etc/systemd/system/
-sudo cp -f /media/usb/services/combi_backend.service /lib/systemd/system/
-sudo cp -f /media/usb/services/wired.network /etc/systemd/network/
-sudo cp -f /media/usb/services/wireless.network /etc/systemd/network/
-sudo cp -f /media/usb/services/wpa_supplicant@wlan0.service /etc/systemd/system/
+# Copy and configure services
+log_message "Copying and configuring services..."
 
-# 8. Asignar permisos a los servicios
-log_message "Asignando permisos a los servicios..." 
-sudo chmod 0644 /etc/systemd/system/storyboard_splash.service
-sudo chmod 0777 /etc/systemd/system/storyboard.service
-sudo chmod 0644 /lib/systemd/system/combi_backend.service
-sudo chmod 0644 /etc/systemd/network/wired.network
-sudo chmod 0644 /etc/systemd/network/wireless.network
-sudo chmod 0644 /etc/systemd/system/wpa_supplicant@wlan0.service
+SERVICES=(
+    "/media/usb/services/storyboard_splash.service:/etc/systemd/system/"
+    "/media/usb/services/storyboard.service:/etc/systemd/system/"
+    "/media/usb/services/combi_backend.service:/lib/systemd/system/"
+    "/media/usb/services/wired.network:/etc/systemd/network/"
+    "/media/usb/services/wireless.network:/etc/systemd/network/"
+    "/media/usb/services/wpa_supplicant@wlan0.service:/etc/systemd/system/"
+)
 
-# 9 Remover los manejadores de conexiones 
-log_message "Removiendo manejadores de conexiones..."
-sudo rm -f /etc/resolv.conf
-sudo rm -f /etc/tmpfiles.d/connman_resolvconf.conf
-sudo systemctl stop connman
-sudo systemctl stop connman-env
-sudo systemctl disable connman
-sudo systemctl disable connman-env
+for service in "${SERVICES[@]}"; do
+    IFS=":" read src dest <<< "$service"
+    sudo cp -f "$src" "$dest"
+    sudo chmod 0755 "$dest"
+done
 
-#10. Activar servicios
-log_message "Activando servicios..."
+# Remove connection handlers
+log_message "Removing connection handlers..."
+sudo rm -f /etc/resolv.conf /etc/tmpfiles.d/connman_resolvconf.conf
+sudo systemctl stop connman connman-env
+sudo systemctl disable connman connman-env
+
+# Enable services
+log_message "Enabling services..."
 sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sudo systemctl stop wpa_supplicant
 sudo systemctl disable wpa_supplicant
-
 sudo systemctl daemon-reload
-sudo systemctl enable storyboard_splash.service
-sudo systemctl enable storyboard.service
-sudo systemctl enable combi_backend.service
-sudo systemctl enable wpa_supplicant@wlan0.service
-sudo systemctl enable systemd-resolved.service
 
-sudo systemctl start storyboard_splash.service
-sudo systemctl start storyboard.service
-sudo systemctl start combi_backend.service
-sudo systemctl start wpa_supplicant@wlan0.service
-sudo systemctl start systemd-resolved.service
+SERVICES_TO_ENABLE=(
+    "storyboard_splash.service"
+    "storyboard.service"
+    "combi_backend.service"
+    "wpa_supplicant@wlan0.service"
+    "systemd-resolved.service"
+)
+
+for service in "${SERVICES_TO_ENABLE[@]}"; do
+    sudo systemctl enable "$service"
+    sudo systemctl start "$service"
+done
 
 
-#11. Cambiar el nombre del servicio weston
-log_message "Cambiando el nombre del servicio weston..." 
-
-if [ -e "/lib/systemd/system/weston.service" ]; then #verifica si existe el archivo
+# Rename weston service
+log_message "Renaming weston service..."
+if [[ -e "/lib/systemd/system/weston.service" ]]; then
     sudo mv /lib/systemd/system/weston.service /lib/systemd/system/weston_Pro_S.service
-    log_message "El nombre del servicio weston fue cambiado correctamente."
-else 
-    log_message "El archivo weston fue cambiado."
+    log_message "The weston service was renamed successfully."
+else
+    log_message "The weston service file was already renamed."
 fi
 
-#12. Copiar contenido de la carpeta "Application" a /usr/crank/apps/Pro-Services"
-log_message "Copiando la versión $version a la carpeta apps..." 
-
-if [ "$operation" = "update" ]; then
-    log_message "Actualizando la aplicación"
-    if [ -z "$LATEST_VERSION" ]; then
+# Update or rollback the application
+log_message "Copying version $version to the apps directory..."
+if [[ "$operation" == "update" ]]; then
+    log_message "Updating the application..."
+    if [[ -z "$LATEST_VERSION" ]]; then
         log_message "No versions found in $APP_PATH"
         exit 1
     else
-        sudo cp -f -r $APP_PATH/$LATEST_VERSION/* $APP_DEST
+        sudo cp -f -r "$APP_PATH/$LATEST_VERSION/"* "$APP_DEST"
         log_message "Software version $LATEST_VERSION updated"
     fi
 else
-    log_message "Haciendo rollback a la versión $version"
-    sudo cp -f -r $APP_PATH/$version/* $APP_DEST
+    log_message "Rolling back to version $version..."
+    sudo cp -f -r "$APP_PATH/$version/"* "$APP_DEST"
 fi
 
-#13. Cambiar el logo de arranque
-log_message  "Cambiando el logo de arranque del sistema..."
-cp -f /media/usb/img/logo.bmp /run/media/mmcblk2p1/logo.bmp
+# Change boot logo
+log_message "Changing the system boot logo..."
+sudo cp -f /media/usb/img/logo.bmp /run/media/mmcblk2p1/logo.bmp
 
 #14. Reiniciar
 log_message  "rebooting..."
-#sudo reboot
+sudo reboot
